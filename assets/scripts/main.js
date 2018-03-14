cb072a332d94f1ce3cc694c44ca600c9fd979a2c
Gerardo Cruz <gerardocd1000@gmail.com>
2018-03-14 10:51:59 -0600

    everything is working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,35 +44,65 @@
             var subarray = [nombre, descripcion, categoria, ciudad, estado, facebook, sitioweb, logo, imagen];
             Data.push(subarray);
           });
-          
+
         }
 
-       console.log('wu');
-       getData();
-       console.log(Data);
+        console.log('wu');
+        getData();
+        console.log(Data);
 
-        function loadInfo(estado){
-          var textoMovilidad;
-          var textoDerechosCiviles;
-          var textoMedioAmbiente;
-          var textoOtros;
+        function loadInfo(estado) {
+          var textoMovilidad = '</div> </div><div class="panel panel-default panel-servicios"> <div class="panel-heading "> <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"> <img class="logoServicios" src="http://localhost/wp-content/themes/placeholdernameForMapaWP/dist/images/bicycle.png" /> Movilidad</a>      </h4> </div> <div id="collapse2" class="panel-collapse collapse">';
+          var textoDerechosCiviles = '</div> </div> <div class="panel panel-default panel-servicios"> <div class="panel-heading "> <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"> <img class="logoServicios" src="http://localhost/wp-content/themes/placeholdernameForMapaWP/dist/images/teamwork.png" /> Derechos Civiles</a>      </h4> </div> <div id="collapse3" class="panel-collapse collapse">';
+          var textoMedioAmbiente = '<div id="menuServicios"><div class="panel-group" id="accordion"> <div class="panel panel-default panel-servicios"> <div class="panel-heading "> <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"> <img class="logoServicios" src="http://localhost/wp-content/themes/placeholdernameForMapaWP/dist/images/earth-day.png" /> Medio Ambiente</a>      </h4> </div> <div id="collapse1" class="panel-collapse collapse">';
+          var textoOtros = '</div> </div> <div class="panel panel-default panel-servicios"> <div class="panel-heading "> <h4 class="panel-title"> <a data-toggle="collapse" data-parent="#accordion" href="#collapse4"> <img class="logoServicios" src="http://localhost/wp-content/themes/placeholdernameForMapaWP/dist/images/packing.png" /> Otros</a>      </h4> </div> <div id="collapse4" class="panel-collapse collapse">';
+          var end = '</div></div></div>';
           for (var i = 0; i < Data.length; i++) {
-            if (Data[i][4]===estado){
-              if(Data[i][2]==='movilidad'){
-
-              }else if(Data[i][2]==='medio-ambiente'){
-
-              } else if(Data[i][2]==='derechos-civiles'){
-
-              } else if(Data[i][2]==='otros'){
-                  
+            if (Data[i][4] === (estado + ' ')) {
+              if (Data[i][2] === 'movilidad ') {
+                textoMovilidad += '<div class="panel-body"> <div class="container"> <div class="row"> <div class="col-md-3"> <img src="https://image.flaticon.com/icons/png/512/355/355400.png"> </div> <div class="col-md-9"> <h1>' + Data[i][0] + '</h1> <h2>' + Data[i][3] + ',' + Data[i][4] + '</h2> <p>' + Data[i][4] + '</p>';
+                if (!(Data[i][5] === '' || Data[i][5] === ' ')) {
+                  textoMovilidad += '<a href="' + Data[i][5] + '"><i class="fab fa-facebook-square"></i></a>';
+                }
+                if (!(Data[i][6] === '' || Data[i][6] === ' ')) {
+                  textoMovilidad += '<a href="' + Data[i][6] + '"><i class="fas fa-globe"></i></a>';
+                }
+                textoMovilidad += ' </div> </div> </div> </div>';
+              } else if (Data[i][2] === 'medio-ambiente ') {
+                textoMedioAmbiente += '<div class="panel-body"> <div class="container"> <div class="row"> <div class="col-md-3"> <img src="https://image.flaticon.com/icons/png/512/355/355400.png"> </div> <div class="col-md-9"> <h1>' + Data[i][0] + '</h1> <h2>' + Data[i][3] + ',' + Data[i][4] + '</h2> <p>' + Data[i][4] + '</p>';
+               if (!(Data[i][5] === '' || Data[i][5] === ' ')) {
+                  textoMedioAmbiente += '<a href="' + Data[i][5] + '"><i class="fab fa-facebook-square"></i></a>';
+                }
+                if (!(Data[i][6] === '' || Data[i][6] === ' '))  {
+                  textoMedioAmbiente += '<a href="' + Data[i][6] + '"><i class="fas fa-globe"></i></a>';
+                }
+                textoMedioAmbiente += ' </div> </div> </div> </div>';
+              } else if (Data[i][2] === 'derechos-civiles ') {
+                textoDerechosCiviles += '<div class="panel-body"> <div class="container"> <div class="row"> <div class="col-md-3"> <img src="https://image.flaticon.com/icons/png/512/355/355400.png"> </div> <div class="col-md-9"> <h1>' + Data[i][0] + '</h1> <h2>' + Data[i][3] + ',' + Data[i][4] + '</h2> <p>' + Data[i][4] + '</p>';
+               if (!(Data[i][5] === '' || Data[i][5] === ' ')) {
+                  textoDerechosCiviles += '<a href="' + Data[i][5] + '"><i class="fab fa-facebook-square"></i></a>';
+                }
+                if (!(Data[i][6] === '' || Data[i][6] === ' ') )  {
+                  textoDerechosCiviles += '<a href="' + Data[i][6] + '"><i class="fas fa-globe"></i></a>';
+                }
+                textoDerechosCiviles += ' </div> </div> </div> </div>';
+              } else if (Data[i][2] === 'otros ') {
+                textoOtros += '<div class="panel-body"> <div class="container"> <div class="row"> <div class="col-md-3"> <img src="https://image.flaticon.com/icons/png/512/355/355400.png"> </div> <div class="col-md-9"> <h1>' + Data[i][0] + '</h1> <h2>' + Data[i][3] + ',' + Data[i][4] + '</h2> <p>' + Data[i][4] + '</p>';
+                if (!(Data[i][5] === '' || Data[i][5] === ' ')) {
+                  textoOtros += '<a href="' + Data[i][5] + '"><i class="fab fa-facebook-square"></i></a>';
+                }
+                console.log(Data[i][6]);
+                if (!(Data[i][6] === '' || Data[i][6] === ' ')) {
+                  textoOtros += '<a href="' + Data[i][6] + '"><i class="fas fa-globe"></i></a>';
+                }
+                textoOtros += ' </div> </div> </div> </div>';
               } else {
                 console.log("error en categoria");
               }
             }
           }
           $('#informacion').fadeOut(function() {
-            $('#informacion').html('<h1>'+estado+'</h1>');
+            $('#informacion').html(textoMedioAmbiente + textoMovilidad + textoDerechosCiviles + textoOtros + end);
             $('#informacion').fadeIn();
           });
         }
@@ -82,7 +112,7 @@
           var estado = $(this).data("title");
           console.log(estado);
           $('.land').removeClass('active');
-          $(this).addClass( "active" );
+          $(this).addClass("active");
           loadInfo(estado);
         });
       },
