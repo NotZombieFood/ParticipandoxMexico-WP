25f475f9fa3abe8d277c306255fac4e001d1bdf5
Gerardo Cruz <gerardocd1000@gmail.com>
2018-03-19 17:41:42 -0600

    added custom post type archive template

--- templates/content-archive-tuitea.php
+++ templates/content-archive-tuitea.php
@@ -1,18 +1,23 @@
 <section id="tuitea-archive-titulo">
-	
+	<h1>¡Tuiteale!</h1>
+	<h2>Conoce las últimas peticiones que se han hecho mediante tuits</h2>
+</section>
+<section id="tuits-archive-posts">
+	<div class="container">
+		<div class="row">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class="col-md-6">
+					<div class="archive-posts-singles">
+					<h1><?php the_title(); ?></h1>
+					<p>
+						<?php the_content(); ?>
+					</p>
+					<a href="<?php echo get_permalink(); ?>">Participar</a>
+					</div>
+				</div>
+			<?php endwhile; // end of the loop. ?>
+		</div>
+	</div>
 </section>
 
 
-
-<?php while ( have_posts() ) : the_post(); ?>
-                 
-<h1 class="entry-title"><?php the_title(); ?></h1>
- 
-<div class="entry-content">
- 
-<?php the_content(); ?>
- 
- 
-</div><!-- .entry-content -->
- 
-<?php endwhile; // end of the loop. ?>
